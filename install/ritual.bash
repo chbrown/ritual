88d5cee5018d5a0442401dbb7f23ee93d81fcadd
Christopher Brown <io@henrian.com>
2015-01-04 23:32:43 -0600

    Don't create global logger; use fact that piping into a function creates a subshell

--- install/ritual.bash
+++ install/ritual.bash
@@ -1,23 +1,31 @@
-# >(logger -t ritual) generates a writable file descriptor that represents syslog
-# a space is required between the N> and >(...) syntax
-exec {RITUAL_LOGGER}> >(logger -t ritual)
-
 function ritual_tcp {
-  # redirect stderr to the logger BEFORE the TCP connection happens,
-  # then put it back, and close the temporary file descriptor.
-  exec {ERR}>&2 2>&$RITUAL_LOGGER {RITUAL}<>/dev/tcp/127.0.0.1/$RITUAL_PORT 2>&$ERR {ERR}>&-
+  # >(logger -t ritual) generates a writable file descriptor that represents
+  # syslog. we'll open up the logger and redirect all STDERR to it, _before_ the
+  # TCP connection happens
+  #exec {STDERR}>&2 # since we only pipe to this function, it'll be in a
+                    # subshell, so we don't have to worry about reseting STDERR
+  exec 2>>>(logger -t ritual)
+  # now try to open up the TCP connection, which may fail, but that output will
+  # go to the logger
+  exec {RITUAL}<>/dev/tcp/127.0.0.1/$RITUAL_PORT
+  # immediately record whether that worked
+  # CONNECTED=$?
+  # if we connected successfully, talk to the RITUAL server
   if [ $? -eq 0 ]; then
-    # send the request from STDIN
-    cat - 2>&$RITUAL_LOGGER 1>&$RITUAL
+    # send the request from STDIN to the TCP socket file descriptor
+    cat - >&$RITUAL
     # send the response to STDOUT
     cat <&$RITUAL
     # even though the ritual server closes the socket, bash will just leave it
     # there hanging, so we have to close it manually.
     exec {RITUAL}>&-
   else
-    echo 'could not establish TCP connection to ritual server' >&$RITUAL_LOGGER
-    return 1
+    echo 'could not establish TCP connection to ritual server' >&2
+    false
   fi
+  #exec 2>&$STDERR {STDERR}>&- # again, since we're in a subshell, we don't have
+                               # to put STDERR back or close the STDERR alias
+  # return $CONNECTED
 }
 
 function ritual_add_pwd {
