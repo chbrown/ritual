4adc60adada381b022c4525cfd9d02a211711ddb
Christopher Brown <io@henrian.com>
2015-02-06 10:05:16 -0600

    Do newline-based communication on the server side, rather than in the handler

--- server.js
+++ server.js
@@ -13,25 +13,29 @@ var server = module.exports = net.createServer(function(socket) {
       if (err) {
         return logger.error('Encountered error: %s', err);
       }
-      logger.debug('Handler result: %s', result);
+      // logger.debug('Handler result: %j', result);
       if (socket.writable) {
-        socket.end(result);
+        socket.write(result + '\n');
       }
     });
   });
   stream.on('error', function(err) {
     logger.error('input stream error: %s', err.message);
   });
+  // stream.on('end', function() {
+  //   socket.end(); // unnecessary
+  // });
 })
 .on('listening', function() {
   var address = server.address();
-  console.log('server listening on tcp://%s:%d', address.address, address.port);
+  logger.info('server listening on tcp://%s:%d', address.address, address.port);
 })
 .on('error', function(err) {
   logger.error('server error: %j', err);
 });
 
 if (require.main === module) {
-  server.listen(parseInt(process.env.RITUAL_PORT) || 0, process.env.RITUAL_HOST);
   logger.level = process.env.RITUAL_VERBOSE ? 'debug' : 'info';
+  logger.info('initializing logger with level: %s', logger.level);
+  server.listen(parseInt(process.env.RITUAL_PORT) || 0, process.env.RITUAL_HOST);
 }
