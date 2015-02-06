6033fd84788dc7c0caa9af096741285bf24f36c2
Christopher Brown <io@henrian.com>
2015-02-06 10:04:15 -0600

    Change entered database type in schema; column types are just suggestions in sqlite3 anyway

--- migrations/01_tables.sql
+++ migrations/01_tables.sql
@@ -1,8 +1,7 @@
 -- I'm gonna try a new thing where I use singular names for table names.
--- this seems to be more of a standard than plural.
+-- this seems to be more of a standard than plural names.
 
 CREATE TABLE directory (
   path TEXT NOT NULL,
-  -- entered TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-  entered TIMESTAMP DEFAULT (strftime('%s', 'now')) NOT NULL
+  entered INTEGER DEFAULT (strftime('%s', 'now')) NOT NULL
 );
