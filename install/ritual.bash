dccb55082bcc91e5aecc1f5b01cd612cba8e32b0
Christopher Brown <io@henrian.com>
2015-12-30 14:35:10 -0600

    Replace echo calls with printf in bash integration

--- install/ritual.bash
+++ install/ritual.bash
@@ -8,30 +8,30 @@ function ritual_tcp {
   exec 3<>/dev/tcp/127.0.0.1/$RITUAL_PORT
   # if we could not connect successfully, return with an error
   if [ $? -ne 0 ]; then
-    echo 'could not establish TCP connection to ritual server' >&2
+    printf 'could not establish TCP connection to ritual server'\\n >&2
     return 1
   fi
   # if we've connected successfully, send STDIN, line by line, to the RITUAL
   # server via the TCP socket file descriptor
   while read -r input; do
-    echo "$input" >&3
-    # echo "sent $input to RITUAL" >&2
+    printf %s\\n "$input" >&3
+    # printf 'sent %s to RITUAL'\\n "$input" >&2
     # read and send the response to STDOUT
     read -r -u 3 output
-    # echo "got response $output" >&2
-    echo "$output"
+    # printf 'got response %s'\\n "$output" >&2
+    printf %s\\n "$output"
   done
   # exec {RITUAL}>&-
 }
 
 function ritual_add_pwd {
   # discard the output (there shouldn't be any)
-  echo '{"action":"add_directory","path":"'$(pwd)'"}' | ritual_tcp >/dev/null
+  printf '{"action":"add_directory","path":"%s"}'\\n "$(pwd)" | ritual_tcp >/dev/null
 }
 
 function j {
   # cd to the output
-  cd "$(echo '{"action":"get_directory","q":"'$@'"}' | ritual_tcp)"
+  cd "$(printf '{"action":"get_directory","q":"%s"}'\\n "$@" | ritual_tcp)"
 }
 
 # set default RITUAL_PORT if not already set
@@ -42,9 +42,9 @@ function j {
 
 # while read -r DIRECTORY; do
 #   if [ ! -d "$DIRECTORY" ]; then
-#     echo Removing missing directory: $DIRECTORY
-#     echo '{"action":"remove_directory","path":"'$DIRECTORY'"}' | ritual_tcp >/dev/null
+#     printf 'Removing missing directory: %s'\\n "$DIRECTORY"
+#     printf '{"action":"remove_directory","path":"%s"}'\\n "$DIRECTORY" | ritual_tcp >/dev/null
 #   fi
-# done < <(echo '{"action":"get_directory_list"}' | ritual_tcp | tr ':' '\n')
+# done < <(printf '{"action":"get_directory_list"}'\\n | ritual_tcp | tr ':' '\n')
 
 PROMPT_COMMAND="ritual_add_pwd"
